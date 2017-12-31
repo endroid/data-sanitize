8f61d9c5bb9450db55454511a2b2116840db59ec
endroid <info@endroid.nl>
2017-12-31 21:22:45 +0100

    Remove bundles

--- src/Bundle/DataSanitizeBundle/Resources/public/src/js/merge.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './components/Application';
-
-ReactDOM.render(
-    <Application loadPath={appConfig.loadPath} mergePath={appConfig.mergePath} />,
-    document.getElementById('application')
-);
