cd7f725c14312aaf1172d4df2eeaed3a33860c99
Jeroen van den Enden <info@endroid.nl>
2020-09-16 10:32:41 +0200

    Set null when target is empty

--- src/Relation.php
+++ src/Relation.php
@@ -33,13 +33,19 @@ final class Relation
 
     public function merge(array $sourceIds, string $targetId): void
     {
+        $targetIdValue = '' === $targetId ? 'NULL' : $targetId;
+
+        // Update existing values with target value
+        // In case of duplicate or invalid NULL ignore
         $query = '
             UPDATE IGNORE '.$this->tableName.'
-            SET '.$this->columnName.' = "'.$targetId.'"
+            SET '.$this->columnName.' = '.$targetIdValue.'
             WHERE '.$this->columnName.' IN ("'.implode('","', $sourceIds).'");';
 
         $this->connection->executeUpdate($query);
 
+        // Make sure failed updates because of duplicate or invalid NULL are deleted
+        // This is the only way to ensure that the source entities can be removed
         $query = '
             DELETE FROM '.$this->tableName.'
             WHERE '.$this->columnName.' IN ("'.implode('","', $sourceIds).'");';
